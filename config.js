4ecdd924d68bc3e8308d9fb2e421fe98bebdefa5
Walinik <roman20060515@gmail.com>
2026-04-25 00:00:38 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// ===== КОНФИГУРАЦИЯ SUPABASE =====
+const SUPABASE_URL = 'https://jwhzfopxfalqhimnbfqw.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3aHpmb3B4ZmFscWhpbW5iZnF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMzMyMTUsImV4cCI6MjA5MjYwOTIxNX0.YNhzwTj6MFgRymR0UyE44Ler2B53T-qfMkZqU6xTp4M';
