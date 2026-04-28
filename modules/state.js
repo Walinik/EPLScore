b4af5ecb0c3ccfdb7a84c6e663bcc741f4ebb223
Walinik <roman20060515@gmail.com>
2026-04-28 11:59:46 +0400

    Update state.js

--- modules/state.js
+++ modules/state.js
@@ -8,3 +8,7 @@ let lastMessageTimestamp = 0;
 let isLoadingMessages = false;
 let isUserAtBottom = true;
 let isSending = false;
+
+// Новые переменные для авто-входа
+let lastUserAutoSave = null;
+const AUTO_LOGIN_TIMEOUT_MS = 5 * 60 * 1000; // 5 минут
